c8d289e03454b0ae6ab6196e65bd77499f487673
Jon Heese <jheese@inetu.net>
2016-05-02 11:30:56 -0400

    Fix database ID fields

--- hardware_sale.sql
+++ hardware_sale.sql
@@ -23,14 +23,14 @@ DROP TABLE IF EXISTS `tbl_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_admin` (
-  `admin_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `admin_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(255) NOT NULL,
   `hashed_password` varchar(255) NOT NULL,
   `admin_email` varchar(255) NOT NULL,
   PRIMARY KEY (`admin_id`),
   UNIQUE KEY `admin_name` (`admin_name`),
   UNIQUE KEY `admin_email` (`admin_email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,16 +41,16 @@ DROP TABLE IF EXISTS `tbl_device`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_device` (
-  `device_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
   `device_name` varchar(255) NOT NULL,
-  `type_id` tinyint(3) unsigned NOT NULL,
+  `type_id` int(255) unsigned NOT NULL,
   `device_description` varchar(1024) DEFAULT NULL,
-  `price` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `price` int(255) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`device_id`),
   UNIQUE KEY `device_id__ukey` (`device_id`),
   KEY `type_id__fkey` (`type_id`),
   CONSTRAINT `tbl_device_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `tbl_type` (`type_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,13 +61,13 @@ DROP TABLE IF EXISTS `tbl_sale`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_sale` (
-  `sale_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `sale_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
   `sale_name` varchar(255) NOT NULL,
   `sale_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `active` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`sale_id`),
   UNIQUE KEY `sale_id` (`sale_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,10 +78,10 @@ DROP TABLE IF EXISTS `tbl_sale_device`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_sale_device` (
-  `sale_device_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `sale_id` tinyint(3) unsigned NOT NULL,
-  `device_id` tinyint(3) unsigned NOT NULL,
-  `quantity` tinyint(4) DEFAULT '0',
+  `sale_device_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `sale_id` int(255) unsigned NOT NULL,
+  `device_id` int(255) unsigned NOT NULL,
+  `quantity` int(255) DEFAULT '0',
   PRIMARY KEY (`sale_device_id`),
   UNIQUE KEY `sale_device_id` (`sale_device_id`),
   UNIQUE KEY `sale_device_id__ukey` (`sale_device_id`),
@@ -89,7 +89,7 @@ CREATE TABLE `tbl_sale_device` (
   KEY `device_id__fkey` (`device_id`),
   CONSTRAINT `tbl_sale_device_ibfk_1` FOREIGN KEY (`sale_id`) REFERENCES `tbl_sale` (`sale_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tbl_sale_device_ibfk_2` FOREIGN KEY (`device_id`) REFERENCES `tbl_device` (`device_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,11 +100,11 @@ DROP TABLE IF EXISTS `tbl_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_type` (
-  `type_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `type_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
   `type_name` varchar(255) NOT NULL,
   PRIMARY KEY (`type_id`),
   UNIQUE KEY `type_id` (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,12 +115,12 @@ DROP TABLE IF EXISTS `tbl_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_user` (
-  `user_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
   `user_email` varchar(255) NOT NULL,
   `active` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_email`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,9 +131,9 @@ DROP TABLE IF EXISTS `tbl_user_sale_device`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_user_sale_device` (
-  `user_sale_device_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` tinyint(3) unsigned NOT NULL,
-  `sale_device_id` tinyint(3) unsigned NOT NULL,
+  `user_sale_device_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) unsigned NOT NULL,
+  `sale_device_id` int(255) unsigned NOT NULL,
   `won` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_sale_device_id`),
   UNIQUE KEY `user_sale_device_id__ukey` (`user_sale_device_id`),
@@ -142,7 +142,7 @@ CREATE TABLE `tbl_user_sale_device` (
   KEY `user_id__fkey` (`user_id`),
   CONSTRAINT `tbl_user_sale_device_ibfk_1` FOREIGN KEY (`sale_device_id`) REFERENCES `tbl_sale_device` (`sale_device_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tbl_user_sale_device_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=207 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,17 +153,17 @@ DROP TABLE IF EXISTS `tbl_user_uuid`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_user_uuid` (
-  `user_uuid_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` tinyint(3) unsigned NOT NULL,
+  `user_uuid_id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) unsigned NOT NULL,
   `uuid` varchar(255) NOT NULL,
-  `sale_device_id` tinyint(3) unsigned NOT NULL,
+  `sale_device_id` int(255) unsigned NOT NULL,
   PRIMARY KEY (`user_uuid_id`),
   UNIQUE KEY `user_uuid_id__ukey` (`user_uuid_id`),
   KEY `user_id__fkey` (`user_id`),
   KEY `tbl_user_uuid_ibfk_2` (`sale_device_id`),
   CONSTRAINT `tbl_user_uuid_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tbl_user_uuid_ibfk_2` FOREIGN KEY (`sale_device_id`) REFERENCES `tbl_sale_device` (`sale_device_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -218,4 +218,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-28  8:38:37
+-- Dump completed on 2016-05-02 11:26:53
